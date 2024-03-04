bf9e9a88cca76128b55a5b945b9a2655527cc2b9
DariaPanchuk <daria_panchuk@ukr.net>
2024-03-04 11:56:44 +0200

    add css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,10 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectAuth } from '../../redux/auth/selectors';
-import UserMenu from '../UserMenu/UserMenu';
-import AuthNav from '../AuthNav/AuthNav';
-import clsx from 'clsx';
-import css from './Navigation.module.css';
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectAuth } from "../../redux/auth/selectors";
+import UserMenu from "../UserMenu/UserMenu";
+import AuthNav from "../AuthNav/AuthNav";
+import clsx from "clsx";
+import css from "./Navigation.module.css";
 
 const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
@@ -14,12 +14,27 @@ const Navigation = () => {
     const { isLoggedIn } = useSelector(selectAuth);
 
     return (
-        <nav className={css.nav}>
-            <NavLink to="/" className={buildLinkClass}>
-                Home
-            </NavLink>
-            {isLoggedIn ? <UserMenu /> : <AuthNav />}
-        </nav>
+    <nav>
+        <div className={css.header}>
+            <div className={css.headerContainer}>
+                <input className={css.checkbox} type="checkbox" name="" id="" />
+                <div className={css.hamburgerLines}>
+                <span className={css.line1}></span>
+                <span className={css.line2}></span>
+                <span className={css.line3}></span>
+                </div>  
+            <div className={css.logo}>
+                <h1>Navbar</h1>
+            </div>
+            <div className={css.menuItems}>
+                <NavLink to="/" className={buildLinkClass}>
+                    Home
+                </NavLink>
+                {isLoggedIn ? <UserMenu /> : <AuthNav />}
+            </div>
+            </div>
+        </div>
+    </nav>
     );
 };
 
